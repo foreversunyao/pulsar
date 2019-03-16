ba4238a4ae8afd9d7cb01be0487968c401f9f35a
yao-sun2 <yao_sun2@apple.com>
2019-03-16 05:53:22 -0700

    pip28 add recycle

--- pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/DirectProxyHandler.java
+++ pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/DirectProxyHandler.java
@@ -189,8 +189,11 @@ public class DirectProxyHandler {
                 cmd = cmdBuilder.mergeFrom(cmdInputStream, null).build();
                 buffer.writerIndex(writerIndex);
                 cmdInputStream.recycle();
-                //System.out.println(".......inDirectProxy,,,"+cmd.getType());
+                System.out.println(".......inDirectProxy,,,"+cmd.getType());
                 System.out.println("..buffer to send.."+buffer.toString(StandardCharsets.UTF_8));
+                cmdBuilder.recycle();
+                cmd.recycle();
+                buffer.release();
                 inboundChannel.writeAndFlush(msg).addListener(this);
                 break;
 
