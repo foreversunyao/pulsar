5285d69976543d3d2a17650cf51538893adbe97a
yao-sun2 <yao_sun2@apple.com>
2019-03-17 20:15:32 -0700

    remove get

--- pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/DirectProxyHandler.java
+++ pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/DirectProxyHandler.java
@@ -180,7 +180,7 @@ public class DirectProxyHandler {
 
                 ByteBufCodedInputStream cmdInputStream = ByteBufCodedInputStream.get(buffer);
                 cmdBuilder = PulsarApi.BaseCommand.newBuilder();
-                //cmd = cmdBuilder.mergeFrom(cmdInputStream, null).build();
+                cmd = cmdBuilder.mergeFrom(cmdInputStream, null).build();
                 System.out.println();
                 //System.out.println("#HandshakeCompleted..cmd"+cmd.getType());
                 inboundChannel.writeAndFlush(msg).addListener(this);
