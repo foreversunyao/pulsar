7e7d121212bc8035869d0d4598a2765d7f584c2a
yao-sun2 <yao_sun2@apple.com>
2019-03-20 04:27:50 -0700

    retain

--- pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/DirectProxyHandler.java
+++ pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/DirectProxyHandler.java
@@ -340,7 +340,7 @@ public class DirectProxyHandler {
             System.out.println("new DirectProxyHandler operationComplete...");
             if (future.isSuccess()) {
                 System.out.println("will do outboundChannel.read()....");
-                outboundChannel.read();
+                //outboundChannel.read();
             } else {
                 log.warn("[{}] [{}] Failed to write on proxy connection. Closing both connections.", inboundChannel,
                         outboundChannel, future.cause());
