f7117710f5ba56df37e2f8c8d4e2a8604819ee47
yao-sun2 <yao_sun2@apple.com>
2019-03-21 22:32:53 -0700

    int cmdSize = (int) buffer.readUnsignedInt();

--- pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/ProxyConnection.java
+++ pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/ProxyConnection.java
@@ -168,8 +168,8 @@ public class ProxyConnection extends PulsarHandler implements FutureListener<Voi
                 ProxyService.bytesCounter.inc(((ByteBuf) msg).readableBytes());
             }
             ByteBuf buffer = (ByteBuf)msg;
-            //int cmdSize = (int) buffer.readUnsignedInt();
-            //System.out.println(".....cmdSize:"+ cmdSize);
+            int cmdSize = (int) buffer.readUnsignedInt();
+            System.out.println(".....length:"+ cmdSize);
             //int writerIndex = buffer.writerIndex();
 
 //            System.out.println(".....length:"+ (int) buffer.readUnsignedInt());
