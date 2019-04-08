40ba5804b6374d7fad85b451eb8d148f6e51f2bf
yao-sun2 <yao_sun2@apple.com>
2019-04-08 02:51:41 -0700

    pip28-v2-debug

--- pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/ParserProxyHandler.java
+++ pulsar-proxy/src/main/java/org/apache/pulsar/proxy/server/ParserProxyHandler.java
@@ -203,6 +203,7 @@ public class ParserProxyHandler extends ChannelInboundHandlerAdapter {
                     while(buffer.readableBytes()>0){
                         msgSize = buffer.readInt();
                         ByteBuf bufferSubMsg = buffer.slice(buffer.readerIndex()-4,buffer.readerIndex()+msgSize);
+                        System.out.println(buffer.readerIndex()-4+"#"+buffer.readerIndex()+msgSize+"#"+bufferSubMsg.readerIndex());
 
                         bufferSubMsg.skipBytes(4);
                         cmdMsgSize=bufferSubMsg.readInt();
